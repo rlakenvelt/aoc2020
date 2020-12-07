c0182b9732d714747a5da495dbbf1597290992eb
Roel <roel@lakenvelt.nl>
2020-12-07 09:00:24 +0100

    Day 7

---
+++ day07/day7a.js
@@ -0,0 +1,38 @@
+
+const shared = require('../common/base.js');
+
+let answer = 0;
+shared.start("day 7A");
+const bags = shared.getInput()
+                    .map(rule => {
+                        const words = rule.split(' ');
+                        const bag = {color: words[0] + ' ' + words[1], contains: []}
+                        for (let index = 4; index < words.length; index+=4) {
+                            if (words[index] !== 'no') {
+                                bag.contains.push({color: words[index + 1] + ' ' + words[index + 2], count: parseInt(words[index])});
+                            }
+                        }
+                        return bag;
+                    });
+
+answer = [...new Set(bagsContaining('shiny gold'))].length;
+
+
+function bagsContaining(color) {
+    const list = bags.filter(bag => bag.contains.some(a => a.color===color));
+    let countList = [];
+    if (list.length > 0) {
+        countList = list.map(a => a.color);
+        list.forEach(a => {
+            countList = [...countList, ...bagsContaining(a.color)];
+        });
+    }
+    return countList;
+}
+
+
+
+// console.log(bags);
+
+shared.end(answer);
+
---
+++ day07/day7b.js
@@ -0,0 +1,29 @@
+
+const shared = require('../common/base.js');
+
+let answer = 0;
+shared.start("day 7B");
+const bags = shared.getInput()
+                    .map(rule => {
+                        const words = rule.split(' ');
+                        const bag = {color: words[0] + ' ' + words[1], contains: []}
+                        for (let index = 4; index < words.length; index+=4) {
+                            if (words[index] !== 'no') {
+                                bag.contains.push({color: words[index + 1] + ' ' + words[index + 2], count: parseInt(words[index])});
+                            }
+                        }
+                        return bag;
+                    });
+answer = bagsIncludedIn('shiny gold');
+
+function bagsIncludedIn(color) {
+    let count = 0;                            
+    bags.find(a => a.color === color).contains.forEach(a => {
+        count += a.count + a.count * bagsIncludedIn(a.color);
+    });
+    return count;
+}
+
+
+shared.end(answer);
+
