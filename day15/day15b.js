aad2839e21483417b0d6eaa0ec1935d848c22a17
Roel <roel@lakenvelt.nl>
2020-12-29 10:08:47 +0100

    Day 15 performance. Refactor and changed keyed collection to Map

--- day15/day15b.js
+++ day15/day15b.js
@@ -3,31 +3,23 @@ const shared = require('../common/base.js');
 
 let answer = 0;
 shared.start("day 15B");
-const input = shared.getInput(',');
-let spokenNumbers = input.reduce((list, number, index) => {
-                             list[number] = {number: number, first: true, last: index, prev: 0};
-                             return list;
-                         }, {});
-let index = input.length;
-let last = input[index-1];
-let lastNumber = spokenNumbers[last];
+const input = shared.getNumericInput(',');
+const lastSpoken=new Map();
+for (let i = 0; i<input.length; i++) {
+    lastSpoken.set(input[i], i+1);
+}
+let turn = input.length;
+let newNumber = 0;
+let lastTurn=turn;
 do {
-    let newNumber = 0;
-    if (!lastNumber.first) {
-        newNumber = lastNumber.last - lastNumber.prev;
-    }
-    lastNumber = spokenNumbers[newNumber]; 
-    if (!lastNumber) {
-        lastNumber = {number: newNumber, first: true, last: index, prev: index};
-        spokenNumbers[newNumber] = lastNumber;
-    } else {
-        lastNumber.first = false;
-        lastNumber.prev  = lastNumber.last;
-        lastNumber.last  = index;
-    }
-    index++;
-} while (index < 30000000);
-answer=lastNumber.number;
+    newNumber = turn-lastTurn;
+    turn++;
+    lastTurn = lastSpoken.get(newNumber);
+    if (!lastTurn) lastTurn = turn;
+    lastSpoken.set(newNumber, turn);
+
+} while (turn < 30000000);
+answer=newNumber;
 
 shared.end(answer);
 
