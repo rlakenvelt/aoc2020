e35dbf8263b9b6aaf4cd33d68dc5ff9940369859
Roel <roel@lakenvelt.nl>
2020-12-29 13:18:42 +0100

    Day 15 even faster using Uint32Array

--- day15/day15b.js
+++ day15/day15b.js
@@ -4,9 +4,9 @@ const shared = require('../common/base.js');
 let answer = 0;
 shared.start("day 15B");
 const input = shared.getNumericInput(',');
-const lastSpoken=new Map();
+const lastSpoken=new Uint32Array(30000000);
 for (let i = 0; i<input.length; i++) {
-    lastSpoken.set(input[i], i+1);
+    lastSpoken[input[i]] = i+1;
 }
 let turn = input.length;
 let newNumber = 0;
@@ -14,10 +14,9 @@ let lastTurn=turn;
 do {
     newNumber = turn-lastTurn;
     turn++;
-    lastTurn = lastSpoken.get(newNumber);
+    lastTurn = lastSpoken[newNumber];
     if (!lastTurn) lastTurn = turn;
-    lastSpoken.set(newNumber, turn);
-
+    lastSpoken[newNumber]=turn;
 } while (turn < 30000000);
 answer=newNumber;
 
